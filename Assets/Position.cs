65929739af64b718e711e674abedb1a03d50a760
Eugene11146 <zhenjaremizov@rambler.ru>
2022-03-14 09:19:50 +0700

    Fixed

--- Assets/Position.cs
+++ Assets/Position.cs
@@ -1,92 +1,65 @@
 using System.Collections;
 using UnityEngine;
-using System.Net.Http;
 using UnityEngine.UI;
-
-
-
 public class Position : MonoBehaviour
 {
-   
-    private Sprite sp;
-    
-    public static Sprite spit;
-    
-    public Sprite spNull;
-
-    private GameObject[] images;
+    private Sprite _currentSprite;
+    public static Sprite bigViewStaticSprite;
+    public Sprite beginSpriteBigView;
+    private GameObject[] _images;
     private int _numberPicture = 1;
-
     public static bool panelActivating;
+    [SerializeField ] private GameObject _picturePanelBigView;
 
-    [SerializeField ] private GameObject picPanel;
-
-
-
-    // т.к. загрузка занимает какое-то время, то используется сопрограмма, позволяющая
-    // вернуться к выполнению метода после завершения WWW запроса
-    // все сопрограммы в C# имеют тип возвращаемого значения - IEnumerator
+    // т.к. загрузка занимает какое-то время, то используется сопрограмма WWW
     IEnumerator Start()
     {
         int i = 0;
-        for (int y = 0; y < images.Length; y++)
+        for (int y = 0; y < _images.Length; y++)
         {
-            if (images[y].tag == "Player")
+            if (_images[y].tag == "Player")
             {
-
                 string url = "http://data.ikppbb.com/test-task-unity-data/pics/" + _numberPicture + ".jpg";
                 // создаем запрос на загрузку текстуры
-
+                
                 if (_numberPicture <= 66)
                 {
                     WWW www = new WWW(url);
                     // ожидаем завершения запроса
                     yield return www;
                     // устанавливаем загруженную текстуру в материал текущего объекта
-                    Texture2D a = www.texture;
-
-                    Debug.Log(url);
-
-                    sp = Sprite.Create(a, new Rect(0, 0, 1000, 1000), new Vector2(), 100);
-
-                    images[y].GetComponent<Image>().sprite = sp;
-
+                    Texture2D downloadTexture = www.texture;
+                    //Формируем новый спрайт из загруженной текстуры
+                    _currentSprite = Sprite.Create(downloadTexture, new Rect(0, 0, 1000, 1000), new Vector2(), 100);
+                    //Вешаем данных спрайт на текущий спрайт из общего массива
+                    _images[y].GetComponent<Image>().sprite = _currentSprite;
                     _numberPicture += 1;
-
-                    Debug.Log(_numberPicture);
                 }
             }
-            
         }
-
-
     }
 
     private void Awake()
     {
-        images = FindObjectsOfType<GameObject>();
-
-        spit = spNull;
+        _images = FindObjectsOfType<GameObject>();
+        bigViewStaticSprite = beginSpriteBigView;
     }
 
     public void ScaledPicture()
     {
         if(panelActivating == true)
         {
-            picPanel.gameObject.GetComponent<Image>().sprite = spit;
-            picPanel.SetActive(true);
+            _picturePanelBigView.gameObject.GetComponent<Image>().sprite = bigViewStaticSprite;
+            _picturePanelBigView.SetActive(true);
         }
     }
-
     public void OffPic()
     {
         panelActivating = false;
-        picPanel.SetActive(false);
+        _picturePanelBigView.SetActive(false);
     }
-
     private void Update()
     {
         ScaledPicture();
-       
     }
 }
